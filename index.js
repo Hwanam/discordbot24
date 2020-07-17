d7452d0c44bff35fd1112483e41fbebe9f18c763
Hwanam <bjmrdia@naver.com>
2020-07-18 02:53:23 +0900

    bot upload

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const token = 'NzMzNjY5NTc2OTk0MzI0NTQw.XxHjKg.uzASNWB98gkZJDcpBzfyTIVvHUA';
+const token = procces.env.token;
 const welcomeChannelName = "안녕하세요";
 const byeChannelName = "안녕히가세요";
 const welcomeChannelComment = "어서오세요.";
